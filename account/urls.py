6c924e1d216e761d42862e45058f96262710fcfa
rogerortiza <rortiz@contractroom.com>
2020-06-20 11:39:27 -0500

    Adding user registration and password change option

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import *
 
 urlpatterns = [
-    path('login/', user_login, name='login'),
+    #path('login/', user_login, name='login'),
+    path('', dashboard, name='dashboard'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_change', auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password/change/done',
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
 ]
