d3716772c8b623fef90adde2d1433d28ec3236c2
rogerortiza <rortiz@contractroom.com>
2020-06-24 23:12:18 -0500

    New ajax request in order to add like/unlike behavior for images

--- images/urls.py
+++ images/urls.py
@@ -6,4 +6,5 @@ app_name = 'images'
 urlpatterns = [
     path('create/', image_create, name='create'),
     path('detail/<int:id>/<slug:slug>/', image_detail, name='detail'),
+    path('like/', image_like, name='like'),
 ]
