8de807da83be4281c91d81d858bd3b63eb7dd987
rogerortiza <rortiz@contractroom.com>
2020-06-25 23:00:49 -0500

    Ajax view to follow users

--- account/urls.py
+++ account/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('edit/', edit, name='edit'),
     path('users/', user_list, name='user_list'),
+    path('users/follow/', user_follow, name='user_follow'),
     path('users/<username>/', user_detail, name='user_detail'),
 ]
