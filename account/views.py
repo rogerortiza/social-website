377d60a44ef6ede00dfab996ee3bda5e795c61e6
rogerortiza <rortiz@contractroom.com>
2020-06-27 17:18:54 -0500

    Optimazing querysets

--- account/views.py
+++ account/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
+from actions.models import Action
+from actions.utils import create_action
 from common.decorators import ajax_required
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, \
     ProfileEditForm
@@ -37,8 +39,17 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
+
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile').prefetch_related('target')[:10]
+
     return render(request, 'account/dashboard.html',
-                  {'section': 'dashboard'})
+                  {'section': 'dashboard',
+                   'actions': actions})
 
 
 def register(request):
@@ -51,6 +62,7 @@ def register(request):
             new_user.save()
 
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
 
             return render(request, 'account/register_done.html',
                           {'new_user': new_user})
@@ -107,6 +119,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
 
